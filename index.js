4486b94c23ff5d5c23563d817c9f9e9177eeb2ec
aaieast <aaieast@outlook.com>
2025-11-02 00:55:20 -0700

    Update index.js

--- index.js
+++ index.js
@@ -75,16 +75,12 @@ export class WebSocketHibernationServer extends DurableObject {
 		
 		try { data = JSON.parse(data); }
 		catch { return false; }
-
+		
 		if (session.server !== undefined) { 
 			let client = this.clients[data.client];    
 			if (!session.server) {
-				if (data.server && !data.key) {
-					client.serializeAttachment({client:data.client, server:data.server});
-					this.sessions.set(client, {client:data.client, server:data.server});
-					this.servers[data.server] = client;
-					data = {msg:0}; // Server ID confirmed.
-				} else data = {msg:2}; // Invalid key.
+				if (data.server && !data.key) return updateMeta(client, data);
+				else client.send(JSON.stringify({msg:2})); // Invalid key.
 			}
 			delete data.client;
 			if (client) client.send(JSON.stringify(data));
@@ -96,13 +92,16 @@ export class WebSocketHibernationServer extends DurableObject {
 		} else if (data.key !== undefined) {
 			data.client = session.client;
 			if (this.servers[0]) this.servers[0].send(JSON.stringify(data));
-			else if (!data.server && data.key == "secret") {
-				ws.serializeAttachment({client:session.client, server:0});
-				this.sessions.set(ws, {client:session.client, server:0});
-				this.servers[0] = ws;
-				ws.send(JSON.stringify({msg:0})); // Server ID confirmed.
-			} else ws.send(JSON.stringify({msg:2})); // Invalid key.
+			else if (!data.server && data.key == "secret") return updateMeta(ws, data);
+			else ws.send(JSON.stringify({msg:2})); // Invalid key.
 		} else ws.send(JSON.stringify({msg:1})); // Server offline.
+		
+		function updateMeta(ws, data) {
+			ws.serializeAttachment({client:data.client, server:data.server});
+			this.sessions.set(ws, {client:data.client, server:data.server});
+			this.servers[data.server] = ws;
+			ws.send(JSON.stringify({msg:0})); // Server ID confirmed.
+		}
 	}
 
 	async webSocketClose(ws, code, reason, wasClean) {
